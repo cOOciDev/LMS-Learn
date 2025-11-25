c50db4ee61e78072e80c2565cabe4494f65b72e0
Navy-M <developermehdieb@gmail.com>
2025-11-25 12:09:42 +0330

    update footer

--- client/src/components/instructor-view/footer.jsx
+++ client/src/components/instructor-view/footer.jsx
@@ -4,7 +4,7 @@ function InstructorFooter() {
   return (
     <footer className="px-4 lg:px-6 h-14 flex items-center justify-center border-t bg-white">
       <div className="text-sm text-muted-foreground">
-        © {currentYear} LMS Learn. All rights reserved.
+        © {currentYear} cOOciDev LMS. All rights reserved.
       </div>
     </footer>
   );
