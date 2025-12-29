8762d944cb1689506a7c9659f79f797a24e24f57
cOOci <109303543+cOOciDev@users.noreply.github.com>
2025-12-29 10:57:35 +0330

    Update index.jsx

--- client/src/pages/student/course-details/index.jsx
+++ client/src/pages/student/course-details/index.jsx
@@ -5,6 +5,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
